3d593f6ee7d71fd459366ce6d63afbbad506eb09
Yk.Zhang <yk.zhang@medias-MacBook-Pro.local>
2018-07-14 19:32:01 +0900

    add talk script for open-jtalk

---
+++ talk-open-jTalk-mac.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+TMP=/tmp/jsay.wav
+VOICE_NAME="$1"
+ALL_VOICE_PATH=`find ~/ -name *"$VOICE_NAME"*.htsvoice`
+# echo $ALL_VOICE_PATH
+for var in $ALL_VOICE_PATH
+do
+    VOICE_PATH="$var"
+    echo $VOICE_PATH
+    break
+done
+echo "$2" | open_jtalk \
+-m $VOICE_PATH \
+-x "/Users/yk.zhang/Programming/openProject/open-jtalk/open_jtalk_dic_utf_8-1.10/" \
+-ow $TMP && \
+afplay $TMP
+rm -f $TMP
