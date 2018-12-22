5eee2214c5ff61f63698f40a45e3c97b7e2d10dc
Hiromitsu Yamaguchi <yamagame@mac.com>
2018-12-22 11:38:08 +0900

    微修正

--- setup-system.sh
+++ setup-system.sh
@@ -15,4 +15,4 @@ sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 sudo apt-get install mecab libmecab-dev mecab-ipadic-utf8 -y
 sudo apt-get install ibus-anthy -y
 sudo apt-get install ntp -y
-sudo apt-get install sqlite3
+sudo apt-get install sqlite3 -y
